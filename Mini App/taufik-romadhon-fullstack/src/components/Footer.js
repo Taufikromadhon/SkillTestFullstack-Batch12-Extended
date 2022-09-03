dc1b57e958367f0b8dff9c03ba2f2f53d911f98c
taufikromadhon <romadhonv26@gmail.com>
2022-09-03 20:26:27 +0700

    third commit, adding true format

---
+++ Mini App/taufik-romadhon-fullstack/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p> Personal Notes - Taufik Romadhon </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Mini App/taufik-romadhon-fullstack/src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+      <p> Personal Notes - Taufik Romadhon </p>
+    </nav>
+  );
+}
+
+export default Nav;
